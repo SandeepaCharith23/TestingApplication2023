ad564c3694f295e37af18b54471c86b46782cf24
Sandeepa Charith <74694596+SandeepaCharith23@users.noreply.github.com>
2023-03-30 21:27:36 +0530

    Log In process Completed

---
+++ lib/src/features/authentication/controllers/signin_contrllers.dart
@@ -0,0 +1,31 @@
+import 'package:ecommerseapp2023/src/features/authentication/screens/dashboard_screen/dashboard_screen.dart';
+import 'package:ecommerseapp2023/src/repository/user_repository/user_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SignInController extends GetxController {
+  static SignInController get instance => Get.find();
+
+  final passwordController = TextEditingController();
+  final firstNameController = TextEditingController();
+
+  final userRepo = Get.put(UserRepository());
+
+  Future<void> usersignInWithEmailandPassword(
+      String email, String password) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      Get.to(() => const DashboardScreen());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        Get.snackbar("user-not-found", "This user cannot find in Database");
+      } else if (e.code == 'wrong-password') {
+        Get.snackbar(
+            "Wrong Password", "Please check your Username and Password");
+      }
+    }
+  }
+}
--- lib/src/features/authentication/screens/loging_screen/login_form.dart
+++ lib/src/features/authentication/screens/loging_screen/login_form.dart
@@ -1,4 +1,5 @@
-import 'package:ecommerseapp2023/src/features/authentication/screens/dashboard_screen/dashboard_screen.dart';
+import 'package:ecommerseapp2023/src/features/authentication/controllers/signin_contrllers.dart';
+
 import 'package:ecommerseapp2023/src/features/authentication/screens/forgetpassword_screen/forgetpassword_options/forgetpassword_model_bottom_sheet.dart';
 
 import 'package:flutter/material.dart';
@@ -11,12 +12,18 @@ class LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controllers = Get.put(SignInController());
+
+    final formkey1 = GlobalKey<FormState>();
+
     return Form(
+      key: formkey1,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 20.0),
         child: Column(
           children: [
             TextFormField(
+              controller: controllers.firstNameController,
               decoration: const InputDecoration(
                 prefixIcon: Icon(
                   Icons.person_outline_outlined,
@@ -25,11 +32,22 @@ class LoginForm extends StatelessWidget {
                 hintText: "Enter Your Email address",
                 border: OutlineInputBorder(),
               ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please Enter your Email address';
+                }
+                if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                    .hasMatch(value)) {
+                  return 'Please enter a valid email';
+                }
+                return null;
+              },
             ),
             const SizedBox(
               height: 10,
             ),
             TextFormField(
+              controller: controllers.passwordController,
               decoration: InputDecoration(
                 prefixIcon: const Icon(
                   Icons.password_outlined,
@@ -42,6 +60,24 @@ class LoginForm extends StatelessWidget {
                   icon: const Icon(Icons.remove_red_eye_outlined),
                 ),
               ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter Your Password';
+                }
+                if (value.length < 8) {
+                  return 'Password must be at least 8 characters long';
+                }
+                // if (!RegExp(r'[a-zA-Z]').hasMatch(value)) {
+                //   return 'Password must contain at least one letter';
+                // }
+                // if (!RegExp(r'[0-9]').hasMatch(value)) {
+                //   return 'Password must contain at least one number';
+                // }
+                // if (!RegExp(r'[!@#\$&*~]').hasMatch(value)) {
+                //   return 'Password must contain at least one special character';
+                // }
+                return null;
+              },
             ),
             const SizedBox(
               height: 10,
@@ -61,7 +97,11 @@ class LoginForm extends StatelessWidget {
               width: double.infinity,
               child: ElevatedButton(
                 onPressed: () {
-                  Get.to(() => const DashboardScreen());
+                  if (formkey1.currentState!.validate()) {
+                    SignInController.instance.usersignInWithEmailandPassword(
+                        controllers.firstNameController.text.trim(),
+                        controllers.passwordController.text.trim());
+                  }
                 },
                 child: const Text("Login"),
               ),
